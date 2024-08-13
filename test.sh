7d5562786ad5b54be5e610391a9d3f6ab5965402
Mr Tom Poek <s4719208@login0.compute.eait.uq.edu.au>
2024-08-13 22:11:35 +1000

    'set -o errexit' to exit script in case of error

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-make clean && 
-make all && 
-./SimulateTraffic_cars trafficCars.csv && 
+set -o errexit
+
+make clean 
+make all 
+./SimulateTraffic_cars trafficCars.csv 
 gprof SimulateTraffic_cars gmon.out > profile.txt
