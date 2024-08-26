43e8eb0de34126ee6ff03f6042281226c054f484
tompoek <tommy.poek@outlook.com>
2024-08-26 11:08:56 +1000

    Option to profile with valgrind cachegrind

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ set -o errexit
 make clean 
 make all 
 ./SimulateTraffic_cars trafficCars.csv 
-gprof SimulateTraffic_cars gmon.out > profile.txt
+# valgrind --tool=cachegrind ./SimulateTraffic_cars trafficCars.csv 
+# gprof SimulateTraffic_cars gmon.out > profile.txt
