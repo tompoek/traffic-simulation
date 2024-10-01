177abd660eb7af07c8a72aed6059effa980b03bd
tompoek <tommy.poek@outlook.com>
2024-10-01 21:05:46 +1000

    At make clean, do not delete all CSVs, delete each in *.sh

--- traffic-two-lanes/test.sh
+++ traffic-two-lanes/test.sh
@@ -2,8 +2,14 @@ set -o errexit
 
 make clean 
 make all 
+
+rm -f TrafficTwoLanes.csv
 ./SimulateTrafficTwoLanes TrafficTwoLanes.csv # filename arg won't work when profiling (if printSteps are commented out)
-# ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv # filename arg won't work when profiling (if printSteps are commented out)
-# ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
 # valgrind --tool=cachegrind ./SimulateTrafficTwoLanes 
 # gprof SimulateTrafficTwoLanes gmon.out > profile.txt
+
+# rm -f TrafficTwoLanes_CUDA.csv
+# ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv # filename arg won't work when profiling (if printSteps are commented out)
+
+# rm -f TrafficTwoLanes_thrust.csv
+# ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
