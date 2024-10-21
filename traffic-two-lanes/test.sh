a3290adc7c4e222d3d793add33b93fd3284389cc
tompoek <tommy.poek@outlook.com>
2024-10-22 09:25:34 +1000

    test script: no need to remove resulted CSVs

--- traffic-two-lanes/test.sh
+++ traffic-two-lanes/test.sh
@@ -3,13 +3,12 @@ set -o errexit
 make clean 
 make all 
 
-rm -f TrafficTwoLanes.csv
-./SimulateTrafficTwoLanes TrafficTwoLanes.csv # filename arg won't work when profiling (if printSteps are commented out)
+# ./SimulateTrafficTwoLanes TrafficTwoLanes.csv # filename arg won't work when profiling (if printSteps are commented out)
 # valgrind --tool=cachegrind ./SimulateTrafficTwoLanes 
 # gprof SimulateTrafficTwoLanes gmon.out > profile.txt
 
-# rm -f TrafficTwoLanes_CUDA.csv
-# ncu -fo report_CUDA.ncu-rep ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv # filename arg won't work when profiling (if printSteps are commented out)
+./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv # filename arg won't work when profiling (if printSteps are commented out)
+# ncu -fo report_CUDA.ncu-rep ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv
 
-# rm -f TrafficTwoLanes_thrust.csv
-# ncu -fo report_thrust.ncu-rep ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
+# ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
+# ncu -fo report_thrust.ncu-rep ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv
