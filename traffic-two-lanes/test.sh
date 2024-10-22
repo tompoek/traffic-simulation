1c95b7cd9bbe133f406217b334c69f652e219ed7
tompoek <tommy.poek@outlook.com>
2024-10-22 16:27:16 +1000

    Update test script to test all codes

--- traffic-two-lanes/test.sh
+++ traffic-two-lanes/test.sh
@@ -3,12 +3,15 @@ set -o errexit
 make clean 
 make all 
 
-# ./SimulateTrafficTwoLanes TrafficTwoLanes.csv # filename arg won't work when profiling (if printSteps are commented out)
+echo "Running CPU code >>>"
+./SimulateTrafficTwoLanes TrafficTwoLanes.csv # filename arg won't work when profiling (if printSteps are commented out)
 # valgrind --tool=cachegrind ./SimulateTrafficTwoLanes 
 # gprof SimulateTrafficTwoLanes gmon.out > profile.txt
 
-# ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
+echo "Running GPU code with thrust >>>"
+./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv # filename arg won't work when profiling (if printSteps are commented out)
 # ncu -fo report_thrust.ncu-rep ./SimulateTrafficTwoLanes_thrust TrafficTwoLanes_thrust.csv
 
+echo "Running GPU code with CUDA manual malloc >>>"
 ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv # filename arg won't work when profiling (if printSteps are commented out)
 # ncu -fo report_CUDA.ncu-rep ./SimulateTrafficTwoLanes_CUDA TrafficTwoLanes_CUDA.csv
